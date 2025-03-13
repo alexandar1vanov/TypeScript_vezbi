29c6c77e95b7a1489de0b04313d3861416a2df1b
Aleksandar Ivanov <aleksandarivanov0907@gmail.com>
2025-03-13 22:27:36 +0100

    made a game

--- vezbi.ts
+++ vezbi.ts
@@ -1,35 +1,70 @@
 
-function add(a: number, b: number): number {
-    return a + b;
+interface Kvadrat {
+  id: number;
+  points: number;
+  speed: number;
+  positionX: number;
+  positionY: number;
+}
+
+const nizaKvadrati: Array<Kvadrat> = new Array();
+var count: number = 0;
+
+function spawnObjects() {
+  count++;
+  const ekran = document.getElementById("game-container");
+  const gameContainer = document.getElementById("game-container");
+  const shirina = gameContainer ? gameContainer.clientWidth : 500;
+  const visina = gameContainer ? gameContainer.clientHeight : 1200;
+
+  const div = document.createElement("div");
+  div.className = "kvadrat";
+  div.id = `kvadrat-${count}`;
+  // console.log(div)
+
+  const pozicijaX: number = Math.floor(Math.random() * shirina);
+  const pozicijaY: number = Math.floor(Math.random() * 250) + visina;    //ODKOMENTIRAJ POSLE
+  const brzina: number = Math.floor(Math.random() * 5);
+  const poeni: number = Math.floor(brzina * 10) 
+
+  const kvadrat4e: Kvadrat = {
+    id: count,
+    points: poeni,
+    speed: brzina,
+    positionX: pozicijaX,
+    positionY: pozicijaY
   }
-  
-  function getAandB() {
-    const a = document.getElementById("a") as HTMLInputElement;
-    const b = document.getElementById("b") as HTMLInputElement;
-    return [a, b];
-  }
-  
-  function setResult(result: string) {
-    const res = document.getElementById("result");
-    if (res != null) {
-      res.innerHTML = result;
+  div.style.left = `${kvadrat4e.positionX}px`;
+  div.style.bottom = `${kvadrat4e.positionY}px`;
+  div.style.opacity = "1";
+
+  nizaKvadrati.push(kvadrat4e);
+
+  ekran?.appendChild(div);
+  // console.log(shirina);
+  console.log(pozicijaY);
+  // console.log(pozicijaX)
+  // console.log('brzina',brzina)
+  // console.log('poeni',poeni)
+  // requestAnimationFrame(startMoving); 
+}
+function startMoving() {
+  // const gameContainer = document.getElementById("game-container");
+  nizaKvadrati.forEach(element => {
+    const kvadratElement = document.getElementById(`kvadrat-${element.id}`);
+    if (kvadratElement) {
+      element.positionY -= element.speed;
+      kvadratElement.style.bottom = `${element.positionY}px`;
     }
+  });
+  requestAnimationFrame(startMoving); 
+}
+
+function startGame() {
+  const kopce = document.getElementById("PlayGameButton");
+  if (kopce != null) {
+    kopce.style.display = "none";
   }
-  
-  const addButton = document.getElementById("btn");
-  if (addButton != null) {
-    addButton.onclick = function () {
-      console.log("on btn click");
-      const [a, b] = getAandB();
-      setResult(add(parseInt(a.value), parseInt(b.value)).toString());
-    };
-  }
-  
-  document.getElementById("mul")!!.onclick = function () {
-    const [a, b] = getAandB();
-    const av = parseInt(a.value);
-    const bv = parseInt(b.value);
-    const result = av * bv;
-    setResult(`${av} x ${bv} = ${result}`);
-  };
-  
+  setInterval(spawnObjects, 1000);
+  requestAnimationFrame(startMoving);
+}
